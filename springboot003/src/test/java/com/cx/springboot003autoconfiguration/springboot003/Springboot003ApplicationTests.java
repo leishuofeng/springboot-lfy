5ec92a4e9f8129ba3366b97b724be86726050b3a
leisf <leishuofeng@aliyun.com>
2019-12-09 22:45:20 +0800

    lfy springboot 19课 自动配置原理，readMe中讲解

---
+++ springboot003/src/test/java/com/cx/springboot003autoconfiguration/springboot003/Springboot003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cx.springboot003autoconfiguration.springboot003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot003ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
